b3c207fc8cb9f2814897a90028362459eed9d085
yaqubadesola <Admin@@2018>
2023-09-25 06:21:26 +0100

    SplitScreen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./SplitScreen";
+const LeftComponent = () => {
+  return <h1 style={{ backgroundColor: "green" }}>LEFT</h1>;
+};
 
+const RightComponent = () => {
+  return <h1 style={{ backgroundColor: "red" }}>RIGHT</h1>;
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <SplitScreen
+    //   left={LeftComponent}
+    //   right={RightComponent}
+    //   leftWeight={1}
+    //   rightWeight={3}
+    // />
+
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftComponent />
+      <RightComponent />
+    </SplitScreen>
   );
 }
 
